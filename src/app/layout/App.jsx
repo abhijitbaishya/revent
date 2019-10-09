bac6c42bc9ce0de9192bd6b8e87e48592954a02e
Abhijit Baishya <abhijitbaishya69@gmail.com>
2019-10-09 20:34:15 +0530

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+yclass App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Re - event</h1>
+      </div>
+    );
+  }
+}
+export default App;
